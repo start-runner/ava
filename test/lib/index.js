05b8055c0c8d82f8ea8ee3f2e3931e3e362f29e0
deepsweet <kir@soulshine.in>
2016-12-26 12:04:35 +0100

    :wrench: switch to `eslint-config-tough`

--- test/lib/index.js
+++ test/lib/index.js
@@ -2,24 +2,24 @@ import test from 'tape';
 
 import task from '../../lib/';
 
-test('basic', t => {
-    t.equal(
-        typeof task,
-        'function',
-        '1st function'
-    );
+test('basic', (t) => {
+  t.equal(
+    typeof task,
+    'function',
+    '1st function'
+  );
 
-    t.equal(
-        typeof task(),
-        'function',
-        '2nd function'
-    );
+  t.equal(
+    typeof task(),
+    'function',
+    '2nd function'
+  );
 
-    t.equal(
-        typeof task()(),
-        'function',
-        '3rd function'
-    );
+  t.equal(
+    typeof task()(),
+    'function',
+    '3rd function'
+  );
 
-    t.end();
+  t.end();
 });
